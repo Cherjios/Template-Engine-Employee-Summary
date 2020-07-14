146bd8ae3c6c64951a33adf26705bf58084ffb45
cherjios <slopez0286@gmail.com>
2020-07-13 23:04:58 -0700

    adding functions to prompt

--- app.js
+++ app.js
@@ -1,18 +1,137 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+// const Manager = require("./lib/Manager");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+// const render = require("./lib/htmlRenderer");
+
+//Adding file 
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What Roll do you want to add?",
+            name: "Roll",
+          }
+    ]);
+  }
+
+function promptManager() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "ID",
+            message: "What is your ID?"
+          },
+          {
+            type: "input",
+            name: "email",
+            message: "Where are you email?"
+          },
+          {
+            type: "input",
+            name: "OfficeNumber",
+            message: "What is your favorite Office Number?"
+          },
+          {
+            type: "input",
+            message: "Do you want to add another team member?",
+            name: "AddMember"
+          }
+    ]);
+  }
+  function promptEngineer() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "ID",
+            message: "What is your ID?"
+          },
+          {
+            type: "input",
+            name: "email",
+            message: "Where are you email?"
+          },
+          {
+            type: "input",
+            name: "Github",
+            message: "What is your Github username?"
+          },
+          {
+            type: "input",
+            message: "Do you want to add another team member?",
+            name: "AddMember"
+          }
+    ]);
+  }
+  function promptIntern() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "ID",
+            message: "What is your ID?"
+          },
+          {
+            type: "input",
+            name: "email",
+            message: "Where are you email?"
+          },
+          {
+            type: "input",
+            name: "School",
+            message: "What is your School?"
+          },
+          {
+            type: "input",
+            name: "AddMember",
+            message: "Do you want to add another team member?"
+            
+          }
+    ]);
+  }
+
+  async function init() {
+    try {
+      var answers = await promptUser();
+
+      if(answers.Roll === 'Manager'){
+          var answerManager = await promptManager();
+      }
+      else if(answers.Roll === 'Engineer'){
+          var answerEngineer = await promptEngineer();
+      }
+      else if(answers.Roll === 'Intern'){
+          var answerIntern = await promptIntern();
+      }
+      if(answerManager.AddMember === 'Yes' || answerEngineer.AddMember === 'Yes' ||answerIntern.AddMember=== 'Yes' ){
+          answer =  promptUser(); 
+      }
+  
+  
+      await writeFileAsync("index.html", answerManager);
+      await writeFileAsync("index2.html", answerEngineer);
+      await writeFileAsync("index3.html", answerIntern);
+  
+      console.log("Successfully wrote to index.html");
+    } catch(err) {
+      console.log(err);
+    }
+  }
+  
+  init();
+  
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
